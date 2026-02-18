8ba5fd65a911c6b71c4312cddf5240be1014a333
ChristopherLui <nospace0831@gmail.com>
2026-02-17 20:35:13 -0500

    Update players.c

--- jeopardy_source/players.c
+++ jeopardy_source/players.c
@@ -13,7 +13,15 @@
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
-    
+ for (int i = 0; i < num_players; i++) 
+    {
+        if (strcmp(players[i].name, name) == 0) 
+        {
+            //player found
+            return true;
+        }
+    }
+    //player not found
     return false;
 }
 
